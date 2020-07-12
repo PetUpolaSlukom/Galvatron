54b3dbb3b423fea91f9c0b4d9dca1bc8b64fbda4
PetUpolaSlukom <61566249+PetUpolaSlukom@users.noreply.github.com>
2020-07-12 12:07:00 +0200

    ispit

--- js/materialize.js
+++ js/materialize.js
@@ -12377,3 +12377,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
+$(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+  });
